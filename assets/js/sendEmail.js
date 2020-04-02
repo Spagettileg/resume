878f5325c541746a74b2f3447680198560cd77ce
Ubuntu <ubuntu@ip-172-31-45-108.ec2.internal>
2020-04-02 14:28:58 +0000

    outlook replaces gamil as the meail provider when sending emails.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) { // Passing in the form as a parameter 
-    emailjs.send("gmail", "paulbf", { // x3 keys below match the email parameters
+    emailjs.send("outlook", "paulbf", { // x3 keys below match the email parameters
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "Project_Request": contactForm.projectsummary.value
