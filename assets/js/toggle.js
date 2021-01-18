1c0972a5f559e2998ea4b82f73d89742d64dd89a
Ubuntu <ubuntu@ip-172-31-36-71.eu-west-1.compute.internal>
2021-01-18 21:10:40 +0000

    Toggle icons animated at 150ms and then animate class removed at end of the animation process.

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,5 +1,12 @@
 // Toggle function to support dark mode
 
 document.querySelector('.toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
+    this.classList.add('animate');
+    
+    setTimeout(() => {
+        this.classList.toggle('active');
+    }, 150);
+    
+    setTimeout(() => this.classList.remove('animate'), 300);
+    
 });
